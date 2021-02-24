680bef42cc0642df83029b84408e472fcf92e506
facu2279 <2279@holbertonschool.com>
2021-02-24 02:12:11 +0000

    Update version

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 import uuid
 from datetime import datetime
 import csv
-from models import storage
+import models
 
 
 class BaseModel:
@@ -11,21 +11,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initializes object"""
-        now = str(datetime.now())
-        now = datetime.strptime(now, "%Y-%m-%d %H:%M:%S.%f")
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at":
-                        value = now
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     if key == "updated_at":
-                        value = now
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns string representation"""
@@ -35,16 +33,12 @@ class BaseModel:
     def save(self):
         """Updates the public instance attr with current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__"""
-        dict_returned = {}
+        dict_returned = dict(self.__dict__)
+        dict_returned["created_at"] = self.created_at.isoformat()
+        dict_returned["updated_at"] = self.updated_at.isoformat()
         dict_returned["__class__"] = self.__class__.__name__
-        for key, value in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                formatx = "%Y-%m-%dT%H:%M:%S.%f"
-                dict_returned[key] = value.strftime(formatx)
-            else:
-                dict_returned[key] = value
-        return (dict_returned)
+        return dict_returned
