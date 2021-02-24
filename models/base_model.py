c35e25b46bbe19b27109e886f63aaa473d90feef
facu2279 <2279@holbertonschool.com>
2021-02-24 02:17:05 +0000

    Fix pep8

--- models/base_model.py
+++ models/base_model.py
@@ -15,10 +15,10 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at":
-                        formatx = "%Y-%m-%dT%H:%M:%S.%f"
-                        value = datetime.strptime(value, formatx)
+                        formatxx = "%Y-%m-%dT%H:%M:%S.%f"
+                        value = datetime.strptime(value, formatxx)
                     if key == "updated_at":
-                        value = datetime.strptime(value, formatx)
+                        value = datetime.strptime(value, formatxx)
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
