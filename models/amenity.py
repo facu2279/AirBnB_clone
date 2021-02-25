916b3cbb4e2ac28fd0259a30161fcb272979a79c
Facundo Diaz <2279@holbertonschool.com>
2021-02-25 17:07:57 +0000

    Delete all

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" Amenity class """
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """ Amenity of the AirBnB """
-    name = ""
--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" City class """
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """ City of the AirBnB """
-    state_id = ""
-    name = ""
